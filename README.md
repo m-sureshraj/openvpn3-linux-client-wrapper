2076240b718fefd3d75370b036efddc5251ffd7b
m-sureshraj <m.s.suresh100@gmail.com>
2021-05-26 15:50:26 +0530

    docs: update readme

--- README.md
+++ README.md
@@ -1,1 +1,54 @@
-# openvpn3-client-wrapper
+# openvpn3-linux-client-wrapper
+A tiny wrapper for the official openvpn3 Linux client. It provides a few
+user-friendly commands to start a session, stop a session, and get status
+of an active session.
+
+## Prerequisites
+* Make sure you have [openvpn3 Linux client](https://github.com/OpenVPN/openvpn3-linux) installed.
+* Make sure you have `Node.js` installed.
+* openvpn connection profile file. i.e `client.ovpn`
+
+## Setup
+1. Navigate to the [release page](https://github.com/m-sureshraj/openvpn3-client-wrapper/releases) and download the latest version of the source code.
+2. Extract the source and copy the `dist/client.js` to wherever you want.
+3. Add the following commands to the shell profile. 
+   i.e `~/.bash_profile`, `~/.zshrc`, `~/.profile`, or `~/.bashrc`
+
+    ```shell
+    export VPN_PROFILE_PATH=</path/to/openvpn/connection/profile>
+    alias vpn='/path/to/downloaded/client.js'
+    ```
+   
+   `VPN_PROFILE_PATH` - Absolute path to the openvpn connection profile file.
+   
+    `alias vpn` - Entry point to the script. By default, the alias name is `vpn` 
+    but you can name it whatever you want. The value should be absolute path to the
+    downloaded `client.js`. (step 2)
+
+4. Source the modified shell profile. e.g. `source .zshrc`
+
+Optionally, you can provide auth credentials (username & password) to the above 
+script to autofill when the start command (to start a new VPN session) prompts 
+for credentials.
+
+```shell
+alias vpn='/path/to/downloaded/client.js username=foo password="abc123"'
+```
+
+Note - If the username or password contains special characters, then you 
+should escape it to prevent parameter expansion. e.g.
+
+`password=foo$dbar` => `password=foo\$dbar`
+
+## Usage
+```
+Usage: vpn [options]
+
+options:
+start   -   start a new session
+stop    -   stop the current session
+status  -   print the session status
+```
+
+## License
+MIT License © [Sureshraj](https://github.com/m-sureshraj)
--- dist/client.js
+++ dist/client.js
@@ -4,17 +4,17 @@ var r=(s,t)=>()=>(t||s((t={exports:{}}).exports,t),t.exports);var i=r((T,u)=>{fu
 `)),process.stdout.write(s.map(t=>`- ${t}`).join(`
 `)),process.stdout.write(`
 
-`)}function N(){process.stdout.write(`Usage: vpn [options]
+`)}function O(){process.stdout.write(`Usage: vpn [options]
 
 `),process.stdout.write(a(`options:
 `)),process.stdout.write(`start   -   start a new session
 `),process.stdout.write(`stop    -   stop the current session
 `),process.stdout.write(`status  -   print the session status
-`)}d.exports={printUsage:N,printSessionsPath:y}});var w=r((j,f)=>{function O(s=[]){let t=s[0]||"";t=t.substring(t.indexOf("=")+1);let e=s[1]||"";return e=e.substring(e.indexOf("=")+1),{username:t,password:e}}f.exports={parseOptions:O}});var S=r((E,x)=>{var{spawnSync:p,spawn:h}=require("child_process"),{printSessionsPath:l}=c(),{red:m,yellow:g,cyan:v}=i(),{parseOptions:C}=w();function b(){let{stdout:s,stderr:t}=p("openvpn3",["sessions-list"],{encoding:"utf8"});return t&&(process.stderr.write(t),process.exit()),s.split(`
+`)}d.exports={printUsage:O,printSessionsPath:y}});var w=r((j,f)=>{function R(s=[]){let t=s[0]||"";t=t.substring(t.indexOf("=")+1);let e=s[1]||"";return e=e.substring(e.indexOf("=")+1),{username:t,password:e}}f.exports={parseOptions:R}});var S=r((M,x)=>{var{spawnSync:p,spawn:h}=require("child_process"),{printSessionsPath:l}=c(),{red:m,yellow:g,cyan:v}=i(),{parseOptions:L}=w();function b(){let{stdout:s,stderr:t}=p("openvpn3",["sessions-list"],{encoding:"utf8"});return t&&(process.stderr.write(t),process.exit()),s.split(`
 `).map(e=>e.trim()).filter(e=>e.startsWith("Path: ")).map(e=>e.split(": ").pop())}function _(){let s=b();s.length===0&&(process.stdout.write(g(`No active sessions found
 `)),process.exit()),s.length>1&&(l(s),process.stdout.write(`Run the following command to get the specific session status
 `),process.stdout.write(v(`openvpn3 session-stats --path <path>
-`)),process.exit());let[t]=s,{stdout:e,stderr:o}=p("openvpn3",["session-stats","--path",t],{encoding:"utf8"});o&&(process.stderr.write(o),process.exit()),process.stdout.write(e)}function R(){let s=b();s.length===0&&(process.stdout.write(g(`No active session found
+`)),process.exit());let[t]=s,{stdout:e,stderr:o}=p("openvpn3",["session-stats","--path",t],{encoding:"utf8"});o&&(process.stderr.write(o),process.exit()),process.stdout.write(e)}function N(){let s=b();s.length===0&&(process.stdout.write(g(`No active session found
 `)),process.exit()),s.length>1&&(l(s),process.stdout.write(`Run the following command to stop a specific session
 `),process.stdout.write(v(`openvpn3 session-manage --disconnect --path <path>
 `)),process.exit());let[t]=s,{stdout:e,stderr:o}=p("openvpn3",["session-manage","--disconnect","--path",t],{encoding:"utf8"});o&&(process.stderr.write(o),process.exit()),process.stdout.write(e)}function W(s,t,e){let o=h("openvpn3",["session-start","--config",e],{encoding:"utf8"});o.stdout.on("data",n=>{process.stdout.write(n),n.toString().trim().toLowerCase().startsWith("auth user name")&&(process.stdout.write(`${s}
@@ -22,5 +22,5 @@ var r=(s,t)=>()=>(t||s((t={exports:{}}).exports,t),t.exports);var i=r((T,u)=>{fu
 `)),n.toString().trim().toLowerCase().startsWith("auth password")&&(process.stdout.write("******"),o.stdin.write(`${t}
 `)),n.toString().trim().toLowerCase().startsWith("enter a passcode")&&(process.stdout.write(`1
 `),o.stdin.write(`1
-`))}),o.stderr.on("data",n=>{process.stderr.write(n)})}function k(s){let t=h("openvpn3",["session-start","--config",s]);t.stdout.pipe(process.stdout),process.stdin.pipe(t.stdin)}function A(s=[]){let{VPN_CONFIG_PATH:t}=process.env;if(t||(process.stderr.write(m("Environment variable `VPN_CONFIG_PATH` not defined. Refer to the setup guide.\n")),process.exit()),s.length===0)k(t);else{let{username:e,password:o}=C(s);(!e||!o)&&(process.stderr.write(m(`username or password is not defined. Refer to the setup guide.
-`)),process.exit()),W(e,o,t)}}x.exports={start:A,stopSession:R,sessionStatus:_}});var{printUsage:L}=c(),{start:U,stopSession:F,sessionStatus:G}=S();function H(){let s=process.argv.slice(2);switch(s.pop()){case"start":U(s);break;case"stop":F();break;case"status":G();break;default:L()}}H();
+`))}),o.stderr.on("data",n=>{process.stderr.write(n)})}function k(s){let t=h("openvpn3",["session-start","--config",s]);t.stdout.pipe(process.stdout),process.stdin.pipe(t.stdin)}function A(s=[]){let{VPN_PROFILE_PATH:t}=process.env;if(t||(process.stderr.write(m("Environment variable `VPN_PROFILE_PATH` not defined. Refer to the setup guide.\n")),process.exit()),s.length===0)k(t);else{let{username:e,password:o}=L(s);(!e||!o)&&(process.stderr.write(m(`username or password is not defined. Refer to the setup guide.
+`)),process.exit()),W(e,o,t)}}x.exports={start:A,stopSession:N,sessionStatus:_}});var{printUsage:C}=c(),{start:E,stopSession:U,sessionStatus:F}=S();function H(){let s=process.argv.slice(2);switch(s.pop()){case"start":E(s);break;case"stop":U();break;case"status":F();break;default:C()}}H();
